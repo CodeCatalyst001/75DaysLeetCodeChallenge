aad0155666b82c739163eb4aaefa8cde2d906314
CodeCatalyst001 <joelvivek1020@gmail.com>
2026-03-16 21:09:18 +0530

    Time: 19 ms (22.49%), Space: 24 MB (26.17%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+from typing import List
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = defaultdict(list)
+        for word in strs:
+            count = [0] * 26
+            for c in word:
+                count [ord(c) - ord('a')] += 1
+            hashmap[tuple(count)].append(word)
+        return list(hashmap.values())
