687fd90b041430038fce574870f7960535b8f7b0
CodeCatalyst001 <joelvivek1020@gmail.com>
2026-03-17 19:23:56 +0530

    Time: 3 ms (88.75%), Space: 22.7 MB (98.15%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,6 @@
+import heapq
 from collections import Counter
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq_map = Counter(nums)
-        n = len(nums)
-        buckets = [[] for _ in range(n + 1)]
-        for num, freq in freq_map.items():
-            buckets[freq].append(num)
-        result = []
-        for i in range(n, 0, -1):
-            for num in buckets[i]:
-                result.append(num)
-                if len(result) == k:
-                    return result
+    def topKFrequent (self, nums, k):
+        count = Counter(nums)
+        return heapq.nlargest(k, count.keys(), key = count.get)
